fd7e639fd4f53752c11f5020812448b97320eadb
Jonathan Sanchez M <jonathan.sanchez@brudifarma.com.mx>
2025-10-01 14:48:23 -0600

    Avance en ejercicios de javascript 1 y 2

--- Practica_Sep_22/codigo.js
+++ Practica_Sep_22/codigo.js
@@ -11,3 +11,27 @@ function covertMetersToKm(){
     console.log(kilometros);
     document.getElementById('cajaKilometros').value = kilometros;
 }
+
+function calculaIMC(){
+     var valorPeso = document.getElementById('inPeso').value;
+     var valorAltura = document.getElementById('inAltura').value;
+     if((valorPeso == null || valorPeso == '') && (valorAltura == null || valorAltura == '')){
+        alert('Debes ingresar un valor para peso y altura!');
+        return;
+     }
+     var peso = parseFloat(valorPeso);
+     var altura = parseFloat(valorAltura);
+     var imc = peso / (altura**2);
+     document.getElementById('outIMC').value = imc;
+}
+
+function calculaGradosF(){
+    var valorC = document.getElementById('inCentigrados').value;
+     if(valorC == null || valorC == ''){
+        alert('Debes ingresar un valor para grados centigrados!');
+        return;
+     }
+     var centigrados = parseFloat(valorC);
+     var cc = (centigrados*9/5) + 32;
+     document.getElementById('outGF').value = cc;
+}
